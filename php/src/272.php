82f60be44b0bca896d1115d7c14cd190eb9165e0
AlvaroLucasPerez <alvarolucasperez@gmail.com>
2023-10-03 00:19:45 +0200

    Test OK

--- php/src/272.php
+++ php/src/272.php
@@ -4,38 +4,32 @@
     <meta charset="UTF-8">
     <title>Ejercicio 272</title>
 </head>
+<body>
 <?php
     include ('atletes.php');
     include ('functions.php');
     global $records;
-?>
-<body>
-<?php
-$fechas = array_column_ext($records,'data',-1);
-$club = array_column_ext($records,'club',-1);
-$ciudades = array_column_ext($records,'lloc',-1);
-$nacimientos = array_column_ext($records,'natalici',-1);
-$atletas = array_column_ext($records,'atleta',-1);
 
-$recordMasAntiguo = vell($fechas);
-$clubMasLaureado = laureado($club);
-$ciudadConMasTitulos = laureado($ciudades);
-$personaMasLaureada = laureado($atletas);
-$atletaMasJoven = jove($nacimientos,$fechas);
-$nombreAtletasMasJoven = '';
-foreach ($records as $record => $datos){
-    if($record == $atletaMasJoven[0]){
-        $nombreAtletasMasJoven = $records[$record]['atleta'];
-    }
-}
+    $datesOfBirth = array_column_ext($records, 'natalici', -1);
+    $clubs = array_column_ext($records, 'club', -1);
+    $cities = array_column_ext($records, 'lloc', -1);
+    $athletes = array_column_ext($records, 'atleta', -1);
+
+    $oldestRecordDate = vell(array_column_ext($records, 'data', -1));
+    $mostSuccessfulClub = laureado($clubs);
+    $mostSuccessfulCity = laureado($cities);
+    $mostSuccessfulAthlete = laureado($athletes);
+    list($youngestAthleteIndex, $youngestAge) = jove($datesOfBirth, array_column_ext($records, 'data', -1));
+    $youngestAthleteName = $records[$youngestAthleteIndex]['atleta'];
 ?>
-<p>Record més antic:  <?= $recordMasAntiguo ?></p>
-<p>Club amb més títols: <?= $clubMasLaureado ?></p>
-<p>Persona amb més records: <?= $personaMasLaureada ?> </p>
-<p>Ciutat més propicia: <?= $ciudadConMasTitulos?></p>
-<p>Mes jove en aconsegir el record: <?= $nombreAtletasMasJoven?> - <?= $atletaMasJoven[1]?> anys</p>
+<p>Record més antic: <?= $oldestRecordDate ?></p>
+<p>Club amb més títols: <?= $mostSuccessfulClub ?></p>
+<p>Ciutat més propicia: <?= $mostSuccessfulCity ?></p>
+<p>Persona amb més records: <?= $mostSuccessfulAthlete ?></p>
+<p>Mes jove en aconsegir el record: <?= $youngestAthleteName ?> - <?= $youngestAge ?> anys</p>
 <?php
-include ('270a.php');
+    include ('270a.php');
 ?>
 </body>
 </html>
+
